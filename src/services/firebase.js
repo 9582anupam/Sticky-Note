03efd471d803c8a670f4ed073c8260c1a0735169
Anupam <9582anupamk@gmail.com>
2024-08-17 13:34:57 +0530

    dotenv added for prod

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -16,15 +16,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB_DEmbotKkUw3qpe7i5kJ4QOva0nuZDTw",
-    authDomain: "sticky-note-9582.firebaseapp.com",
-    projectId: "sticky-note-9582",
-    storageBucket: "sticky-note-9582.appspot.com",
-    messagingSenderId: "604125189156",
-    appId: "1:604125189156:web:81580159c22793ee7b34b1",
-    measurementId: "G-C8L5GFFV4S",
-    databaseURL:
-        "https://sticky-note-9582-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
